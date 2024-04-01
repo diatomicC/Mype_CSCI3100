7232a4683df76c170224addf4ce642e6114ec7fd
mattmaomao <matthewcp220@gmail.com>
2024-04-01 09:07:41 +0800

    added profile, language

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,6 +7,18 @@ function HomePage() {
           <img class="logo-image" src="" alt="icon" />
           <span class="logo-text">Mype</span>
         </div>
+        <div class="header-options">
+          {/* login/ signup/ profile */}
+          <button class="user-profile">
+            <span>User Profile</span>
+          </button>
+          {/* language option */}
+          <button class="lang-setting">
+            <span>Language</span>
+            {/* globe icon */}
+            {/* arrow icon */}
+          </button>
+        </div>
       </div>
     </>
   );
