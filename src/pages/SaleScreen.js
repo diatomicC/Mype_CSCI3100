de241cccca9d421acd218fc64600938487a8dda9
mattmaomao <matthewcp220@gmail.com>
2024-04-01 13:01:25 +0800

    created sale screen page js file

---
+++ src/pages/SaleScreen.js
@@ -0,0 +1,20 @@
+import Header from "../components/Header";
+import MainSearchBar from "../components/HomeSearchBar";
+
+import "../styles/homepage.css";
+
+function SaleScreen() {
+  return (
+    <>
+      {/* Header */}
+      <Header />
+
+      {/* search area*/}
+      <MainSearchBar />
+
+        {/* detailed information of single selected product */}
+    </>
+  );
+}
+
+export default SaleScreen;
