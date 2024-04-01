f60f21f56efc9644a10e1230f319414d0c3601d2
mattmaomao <matthewcp220@gmail.com>
2024-04-01 13:46:32 +0800

    use router lib to switch pages in homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "../components/Header";
 import HomeItemContainer from "../components/HomeItemContainer";
 import MainSearchBar from "../components/HomeSearchBar";
+import SaleScreen from "./SaleScreen";
 
 import "../styles/homepage.css";
 
 function HomePage() {
   return (
     <>
-      {/* Header */}
-      <Header />
+      <Router>
+        {/* Header */}
+        <Header />
+        {/* search area*/}
+        <MainSearchBar />
 
-      {/* search area*/}
-      <MainSearchBar />
-
-      {/* area for display items */}
-      <HomeItemContainer />
+        <Routes>
+          {/* display items match filtering condition */}
+          <Route path="/" element={<HomeItemContainer/>} />
+          
+          {/* display detailed information of single item */}
+          <Route path="/Product" element={<SaleScreen/>} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/pages/SaleScreen.js
+++ src/pages/SaleScreen.js
@@ -1,18 +1,10 @@
-import Header from "../components/Header";
-import MainSearchBar from "../components/HomeSearchBar";
-
 import "../styles/homepage.css";
 
 function SaleScreen() {
   return (
     <>
-      {/* Header */}
-      <Header />
-
-      {/* search area*/}
-      <MainSearchBar />
-
         {/* detailed information of single selected product */}
+        <div>hi</div>
     </>
   );
 }
