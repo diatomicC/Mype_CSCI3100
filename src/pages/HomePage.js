23d716b6a98742693f3a261b63be993d03eb84ed
mattmaomao <matthewcp220@gmail.com>
2024-04-01 11:14:42 +0800

    move homepage item template to new componenet, create blank css for that

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,6 @@
 import Header from "../components/Header";
+import HomeBlockDisplayItem from "../components/HomeBlockDisplayItem";
+
 import SearchIcon from "../icons/searchIcon.svg";
 import BookmarkIcon from "../icons/bookmark.svg";
 import "../styles/homepage.css";
@@ -14,7 +16,7 @@ function HomePage() {
           <div class="search-bar-item">
             {/* toggle to show search field (todo) */}
             <button class="search" id="search">
-            <img src={SearchIcon} alt=""/>
+              <img src={SearchIcon} alt="" />
             </button>
             {/* toggle to filter only/ also(?) saved item */}
             <input class="search-field" type="text" />
@@ -22,7 +24,7 @@ function HomePage() {
           {/* toggle to filter only/ also(?) saved item */}
           <div class="search-bar-item">
             <button class="saved capsule" id="saved">
-              <img src={BookmarkIcon} alt=""/>
+              <img src={BookmarkIcon} alt="" />
               Saved
             </button>
           </div>
@@ -38,49 +40,7 @@ function HomePage() {
 
       {/* area for display items */}
       <div class="content-container">
-        {/* item template */}
-        <div class="item">
-          {/* item header */}
-          <div>
-            <span class="title">title</span>
-            <span class="author">author</span>
-          </div>
-          {/* item content */}
-          <div>
-            {/* click to quick save to shopping cart */}
-            <button class="save-btn">
-              <img class="save-icon" src="" alt="bookmark icon" />
-            </button>
-            <img class="product-image" src="" alt="product image" />
-            {/* todo */}
-            {/* display first x (depends on item frame width?) tags of this product */}
-            <div class="item-tags">
-              <div class="item-tag">#tag 1</div>
-              <div class="item-tag">#tag 2</div>
-              <div class="item-tag">#tag 3</div>
-            </div>
-            <span class="short-description">blablablab</span>
-          </div>
-          {/* interactives */}
-          <div>
-            {/* display number of likes, saved */}
-            <div class="ranking">
-              <div class="liked">
-                <img src="" alt="like icon" /> #like
-              </div>
-              <div class="saved">
-                <img src="" alt="bookmark icon" /> #save
-              </div>
-            </div>
-            {/* click to share/ purchase this product */}
-            <div class="interactives-btn">
-              <button class="share">
-                <img src="" alt="share icon" />
-              </button>
-              <button class="purchase">Buy now</button>
-            </div>
-          </div>
-        </div>
+        <HomeBlockDisplayItem />
       </div>
     </>
   );
