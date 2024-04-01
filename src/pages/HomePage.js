c26deec3a6156200570bb28e109574dcb9c3cd90
mattmaomao <matthewcp220@gmail.com>
2024-04-01 09:54:27 +0800

    added search area

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,21 @@ function HomePage() {
   return (
     <>
       <Header />
+      {/* search area*/}
+      <h4>PROJECT LIST</h4>
+      <div class="search-bar">
+        <button class="seacrh" id="seacrh">
+          <img src="" alt="search icon"></img>
+        </button>
+        <button class="saved" id="saved">
+          <img src="" alt="bookmark icon"></img>Saved
+        </button>
+        <div class="filter-tags">
+          <button class="filter-tag">#tag 1</button>
+          <button class="filter-tag">#tag 2</button>
+          <button class="filter-tag">#tag 3</button>
+        </div>
+      </div>
     </>
   );
 }
