b2fd24e58e7117a1555aeab3fb4233d75772b4c3
mattmaomao <matthewcp220@gmail.com>
2024-04-01 09:07:01 +0800

    added header, logo to  homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,14 @@
 function HomePage() {
   return (
-    <></>
+    <>
+      <div class="header">
+        {/* logo */}
+        <div class="logo">
+          <img class="logo-image" src="" alt="icon" />
+          <span class="logo-text">Mype</span>
+        </div>
+      </div>
+    </>
   );
 }
 
