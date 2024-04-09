c68ed0f198217cdba2b1ebc755ce5e41ce5218b7
swchang2 <1155177186@link.cuhk.edu.hk>
2024-04-10 00:43:56 +0800

    Created Payment Screen

---
+++ src/pages/PaymentScreen.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import SummaryBlock from "../components/payment/SummaryBlock";
+
+function PaymentScreen() {
+  return (
+    <>
+      {/* Header */}
+      <Header />
+      <SummaryBlock></SummaryBlock>
+    </>
+  );
+}
+
+export default PaymentScreen;
