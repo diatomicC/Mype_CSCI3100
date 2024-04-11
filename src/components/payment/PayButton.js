2ebdac05780d2a04ee702a5bb650568a6861ebb6
swchang2 <1155177186@link.cuhk.edu.hk>
2024-04-11 09:49:47 +0800

    update

--- src/components/payment/PayButton.js
+++ src/components/payment/PayButton.js
@@ -1,23 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+import { Link } from "react-router-dom";
+import { getDoc } from "firebase/firestore";
+import { doc } from "firebase/firestore";
+import { updateDoc } from "firebase/firestore";
+import { experimentalSetDeliveryMetricsExportedToBigQueryEnabled } from "firebase/messaging/sw";
 
-function PayButton() {
-  const getAmount = () => {
-    /* TODO */
-    return 200;
+function PayButton({ props, db }) {
+  let [coin, setCoin] = useState(0);
+  // let coin = 0;
+  const FetchUser = async () => {
+    console.log("In fetch");
+    const docRef = doc(db, "User", "KNnrR1hpeBv5Mhytx0Df");
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      coin = docSnap.data().m_coin;
+      setCoin(docSnap.data().m_coin);
+    } else {
+      console.log("Data not found");
+    }
+    return "Done";
   };
+  const Paid = () => {
+    console.log("Run before Fetch");
+    FetchUser().then((coin) => {
+      console.log(coin);
+    });
+    console.log("Start paying section");
+    console.log(coin);
+    if (coin < Number(getAmount())) {
+      return false;
+    } else {
+      console.log("Changing in database m_coin");
+      console.log(getAmount());
+      updateMcoinInDb(db, "KNnrR1hpeBv5Mhytx0Df", Number(getAmount()));
+      return true;
+    }
+  };
+  const updateMcoinInDb = async (db, userID, new_coin) => {
+    const docRef = doc(db, "User", userID);
+    console.log(new_coin);
+    updateDoc(docRef, { m_coin: new_coin });
+  };
+  const getAmount = () => {
+    let total = 0;
+    let i = 0;
+    do {
+      total += props[i].price;
+      i++;
+    } while (i < props.length);
+    return total;
+  };
+  const ClickEvent = () => {
+    console.log("Handling clickevent");
+    if (Paid()) {
+      setPaid(true);
+    } else {
+      setPaid(false);
+    }
+  };
+  let [successPaid, setPaid] = useState(false);
   return (
     <>
-      <button
-        style={{
-          backgroundColor: "#040273",
-          color: "white",
-          margin: "0",
-          padding: "10px",
-          width: "100%",
-        }} /* TODO onClick={} */
+      {" "}
+      <Popup
+        trigger={
+          <button
+            style={{
+              backgroundColor: "#040273",
+              color: "white",
+              margin: "0",
+              padding: "10px",
+              width: "100%",
+            }}
+            onMouseUp={ClickEvent}
+          >
+            Pay ${getAmount()}
+          </button>
+        }
+        modal
+        nested
+        closeOnDocumentClick={false}
       >
-        Pay ${getAmount()}
-      </button>
+        {() =>
+          successPaid ? (
+            <>
+              <div className="paid-pop-up">Your transaction is completed.</div>
+              <Link to="/">Back to home page.</Link>{" "}
+            </>
+          ) : (
+            <>
+              <div className="paid-pop-up">Your transaction is failed.</div>
+              <Link to="/">Back to home page.</Link>
+            </>
+          )
+        }
+      </Popup>
     </>
   );
 }
--- src/components/shoppingCart/ShoppingCartContainer.js
+++ src/components/shoppingCart/ShoppingCartContainer.js
@@ -9,7 +9,7 @@ import {
   updateDoc,
 } from "firebase/firestore";
 import "../../styles/shoppingcart.css";
-import $ from "jquery";
+import $, { data } from "jquery";
 import { Link } from "react-router-dom";
 
 function ShoppingCartContainer({ currentUserId, db }) {
@@ -18,29 +18,44 @@ function ShoppingCartContainer({ currentUserId, db }) {
   useEffect(() => {
     product_list = [];
     setProduct_list([...product_list]);
-    FetchUser().then(FetchProduct());
+    FetchUser()
+      .then((result) => console.log(result))
+      .then(FetchProduct);
     console.log(product_list);
     console.log(shoppingCart);
   }, []);
 
   const FetchUser = async () => {
+    console.log("Start fetching user");
+    console.log(shoppingCart);
     const docRef = doc(db, "User", currentUserId);
     const docSnap = await getDoc(docRef);
-    setShoppingCart(docSnap.data().shopping_cart.split(" "));
+    if (docSnap.data().length === 0) {
+      console.log("Not user");
+    } else {
+      console.log("Finished fetching user");
+      // return docSnap.data().shopping_cart.split(" ");
+      shoppingCart = docSnap.data().shopping_cart.split(" ");
+      setShoppingCart(docSnap.data().shopping_cart.split(" "));
+      return shoppingCart;
+    }
   };
 
   const FetchProduct = async () => {
-    if (!shoppingCart.length === 0) {
-      const q = query(
-        collection(db, "Products"),
-        where("public_ID", "in", shoppingCart)
-      );
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-        product_list.push(doc.data());
-      });
-      setProduct_list([...product_list]);
-    }
+    console.log("Start fetching product");
+    setTimeout(() => {}, 5000);
+    console.log("Finish timeout");
+
+    console.log(shoppingCart);
+    const q = query(
+      collection(db, "Products"),
+      where("public_ID", "in", shoppingCart)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      product_list.push(doc.data());
+    });
+    setProduct_list([...product_list]);
   };
 
   const GetSelected = () => {
@@ -76,9 +91,25 @@ function ShoppingCartContainer({ currentUserId, db }) {
     // updateDoc(docRef, { shopping_cart: {updated_shopping_cart} });
   };
 
+  // let [payment, setPayment] = useState([]);
+  const UpdatePayment = () => {
+    let tmp_selected = GetSelected();
+    product_list.slice().map(function (item, index) {
+      if (!tmp_selected.includes(item.public_ID)) {
+        product_list.splice(index, 1);
+      }
+    });
+
+    shoppingCart.slice().map(function (item, index) {
+      if (!tmp_selected.includes(item)) {
+        shoppingCart.splice(index, 1);
+      }
+    });
+    console.log(product_list);
+  };
+
   return (
     <>
-      <p></p>
       <div className="cart-column">
         <h2>Select Project</h2>
         <button onClick={RemoveCart} disabled={product_list.length === 0}>
@@ -115,8 +146,10 @@ function ShoppingCartContainer({ currentUserId, db }) {
         </>
       ))}
       <div className="cart-column">
-        <Link to="payment">
-          <button disabled={product_list.length === 0}>Go to payment</button>
+        <Link to="/payment" state={product_list}>
+          <button disabled={product_list.length === 0} onClick={UpdatePayment}>
+            Go to payment
+          </button>
         </Link>
       </div>
     </>
