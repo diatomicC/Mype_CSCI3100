a27168cea0bad73824f72b25feadeec4f27baa20
Kim Taehee <108227894+user-nameee@users.noreply.github.com>
2024-04-26 19:19:55 +0800

    added comments

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -6,20 +6,25 @@ import "../../styles/LoginSignUp.css";
 import Header from "../Header";
 
 const SignIn = () => {
+    //variables need for login (sign in), like email and password.  
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     console.log(auth);
 
+    //executed when user submits info
     const handleSubmit = async (e) => {
       e.preventDefault()
       try{
+        //signInWithEmailAndPassword is a function provided by firebase, allowing to login with email and password.
         const user = await signInWithEmailAndPassword(auth, email, password)
         if (user) {
+          //if registration successfully done, notice the user that it's done
           alert("Login Successful")
         }
         console.log("Login Successful")
       } catch (err) {
+        //if there's any error, show the errors as well.
         alert(err)
         console.log(err)
       }
@@ -34,6 +39,10 @@ const SignIn = () => {
             <p>Log in to access your projects, track your activities, and more.</p>
             <br></br>
             <div class="input info">
+              {/*
+                input bars to get users' email and password
+                when user press the submit button, it triggers the function above
+              */}
               <form onSubmit={handleSubmit}>
                 <input id="info" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
                 <br></br>
@@ -43,6 +52,7 @@ const SignIn = () => {
               </form>
             </div>
             <br></br>
+            {/*clicking on this link send the user to sign up page*/}
             <Link to="/signup">Create an account</Link>
           </div>
         </div>
