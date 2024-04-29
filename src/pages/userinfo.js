af069a5e0f1cea13fe08a75ea9b3ed7748268b87
ckhkc <kalvinchu2001@gmail.com>
2024-04-29 23:50:16 +0800

    Update userinfo.js
    added comments, activated the hyperlinks and fixed the bug of not interchangeable

--- src/pages/userinfo.js
+++ src/pages/userinfo.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "../components/Header";
 import {Profile} from "../components/userinfo/profile.js"
 import { Outlet } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export const Userinfo = () => {
   return(
@@ -10,24 +11,42 @@ export const Userinfo = () => {
       <div class="container">
         <div class="row">
           <div class="col-2">
+         /*This is the left side column navigating bar*/
             <ul class="nav flex-column">
+            /*direct to the user profile*/
               <li class="nav-item">
+                <Link to ="profile">
                 <a class="nav-link active" aria-current="page" href="userinfo/profile">Profile</a>
+                </Link>
               </li>
+            /*direct to the product selling of the user*/
               <li class="nav-item">
+                <Link to ="profile">
                 <a class="nav-link active" aria-current="page" href="product">Product</a>
+                </Link>
               </li>
+            /*direct to the shopping cart of the user*/
               <li class="nav-item">
+                <Link to ="shoppingcart">
                 <a class="nav-link active" aria-current="page" href="userinfo/shoppingcart">Shopping Cart</a>
+                </Link>
               </li>
+            /*direct to the order tracking function of the user*/
               <li class="nav-item">
+                <Link to ="profile">
                 <a class="nav-link active" aria-current="page" href="ordertracking">Order tracking</a>
+                </Link>
               </li>
+            /*direct to the currency part of the user*/
               <li class="nav-item">
+                <Link to ="profile">
                 <a class="nav-link active" aria-current="page" href="currency">Currency</a>
+                </Link>
               </li>
               <li class="nav-item">
+                <Link to ="profile">
                 <a class="nav-link active" aria-current="page" href="preference">Preference</a>
+                </Link>
               </li>
             </ul>
           </div>
